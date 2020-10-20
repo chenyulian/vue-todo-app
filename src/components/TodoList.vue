3351e74bcc4e6a243cca44b02f3c0e9b14a9a294
kinppink <648270560@qq.com>
2020-10-20 14:13:52 +0800

    将部分功能模块抽离成组件

---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="todo-list">
+    <todo-item />
+    <todo-item />
+</div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+export default {
+    components: {
+        TodoItem,
+    },
+};
+</script>
+
+<style scoped>
+.todo-list {
+    display: grid;
+    row-gap: 14px;
+}
+</style>
